db9356e4c7ed2e898a6a3fe9cbd0b48a7e5ca603
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2018-05-08 22:33:35 -0700

    Show back button in SettingsActivity

--- app/src/main/java/com/bonnetrouge/vimhelp/Activities/SettingsActivity.kt
+++ app/src/main/java/com/bonnetrouge/vimhelp/Activities/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.bonnetrouge.vimhelp.Activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.bonnetrouge.vimhelp.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -17,5 +18,17 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (item != null && item.itemId == android.R.id.home) {
+            onBackPressed()
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
